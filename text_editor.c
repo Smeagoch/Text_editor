8b8579077e9b7441b54add42a6af1f3b0158eb90
Smeagoch <nniikk2001@gmail.com>
2022-07-06 16:40:20 +0300

    Don't work input and output text

---
+++ text_editor.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define nbuf 128
+
+
+
+int main()
+{
+
+    char ** pStr;
+    int rows = 1;
+    pStr = malloc(rows * sizeof(char *) + rows * nbuf * sizeof(char));
+//    for ( int i = 0; i < rows; i++)
+//        pStr[i] = malloc(nbuf * sizeof(char));
+
+
+    printf ("Input your text: \n");
+    char stopStr[3] = "end";
+    for(int i = 0; i <= rows; i++)
+    {
+	char buffer[nbuf];
+	gets(buffer);
+
+	if (buffer[0] == stopStr[0] && buffer[1] == stopStr[1] && buffer[2] == stopStr[2])
+	    break;
+
+	if( i == rows)
+	{
+	    rows++;
+	    pStr = realloc(pStr, rows * sizeof(char *) + rows * nbuf * sizeof(char));
+	}
+	pStr[i] = buffer;
+    }
+
+    printf("Your text: \n");
+    for (int i = 0; i < rows; i++)
+	printf ("%s %d\n", pStr[i], &pStr[i]);
+
+//    for (int i = 0; i < rows; i++)
+//	free(pStr[i]);
+    free(pStr);
+    return 0;
+}
