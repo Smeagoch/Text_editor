1cafd30d66c1e4b61f6ccb3d16566f648b8a18fc
Smeagoch <nniikk2001@gmail.com>
2022-07-12 19:14:52 +0300

    Fix error of lenght in new string, add null termination of string, clearing the memory allocated for the old string

--- text_editor.c
+++ text_editor.c
@@ -2,10 +2,9 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define nbuf 128
+#define NBUF 128
 
-
-int str_find_word(char * pStr, char ** pWord) // Find word function
+int str_find_word(char *pStr, char **pWord) // Find word function
 {
     int count = 1;
     if (97 <= *pStr && *pStr <= 122) // The condition first lowercase letter
@@ -14,52 +13,51 @@ int str_find_word(char * pStr, char ** pWord) // Find word function
 	count++;
     }
     for (int i = 0; i < strlen(pStr); i++)
-        if (*(pStr + i) == 32 && (97 <= *(pStr + i + 1) && *(pStr + i + 1) <= 122)) // The condition for searching for words with a lowercase letter
+        if (pStr[i] == 32 && (97 <= pStr[i + 1] && pStr[i + 1] <= 122)) // The condition for searching for words with a lowercase letter
 	{
 	    if( count != 1)
 	    {
 		pWord = realloc (pWord, (count) * sizeof(char *));
 	    }
-	    *(pWord + count - 1) = pStr + i + 1;
+	    pWord[count - 1] = pStr + i + 1;
 	    count++;
 	}
-    return count;
+    return count - 1;
 }
 
-
-void text_editor(char * pStr, char **pWord, int nWord) // Text editor function
+char *text_editor(char *pStr, char **pWord, int nWord) // Text editor function
 {
-    char * pdStr = pStr;
-    char ** pdWord = pWord;
+    char *pdStr = pStr;
+    char **pdWord = pWord;
     int len; // Length of new string
     if (nWord > 9)
 	len = (strlen(pStr) + 4 * nWord - 9);
     else
 	len = (strlen(pStr) + 3 * nWord);
-    char pNewStr[len];
+    char *pNewStr = (char *) malloc (len * sizeof(char));
 
     for (int i = 0; i < len; i++)
     {
 	if (pdStr == *pdWord) // The condition for numbering a word in a string
 	{
-			      // Numbering a word
-	    *(pNewStr + i) = 40;
+	    // Numbering a word
+	    pNewStr[i] = 40;
 	    if (pdWord - pWord + 1 > 9)
 	    {
-		*(pNewStr + i + 1) = (pdWord - pWord + 1) / 10 + '0';
+		pNewStr[i + 1] = (pdWord - pWord + 1) / 10 + '0';
 		i++;
 	    }
-	    *(pNewStr + i + 1) = (pdWord - pWord + 1) % 10 + '0';
-	    *(pNewStr + i + 2) = 41;
+	    pNewStr[i + 1] = (pdWord - pWord + 1) % 10 + '0';
+	    pNewStr[i + 2] = 41;
 	    i = i + 3;
 	    pdWord++;
 	}
-	*(pNewStr + i) =  *pdStr; // Copy symbol in string
+	pNewStr[i] =  *pdStr; // Copy symbol in string
 	pdStr++;
     }
-    printf("%s\n", pNewStr);
-    pStr = realloc(pStr, len * sizeof(char));
-    strcpy(pStr,pNewStr);
+    free(pStr);
+    strcat(pNewStr, "\0");
+    return pNewStr;
 }
 
 
@@ -77,8 +75,10 @@ int main()
     // Input text
     for(int i = 0; i <= rows; i++)
     {
-	char buffer[nbuf];
-	gets(buffer); //Filling the buffer
+	char buffer[NBUF];
+	fgets(buffer, NBUF, stdin); //Filling the buffer
+	buffer[strlen(buffer) - 1] = 0;
+//	strcat(buffer, "\0");
 
         // The condition of stop input text
 	if (buffer[0] == stopStr[0] && buffer[1] == stopStr[1] && buffer[2] == stopStr[2])
@@ -104,7 +104,7 @@ int main()
     {
 	char ** pWord = malloc(1 * sizeof(char *));
 	int nWord = str_find_word(pStr[i], pWord);
-	text_editor(pStr[i], pWord, nWord);
+	pStr[i] = text_editor(pStr[i], pWord, nWord);
 	free(pWord);
     }
 
